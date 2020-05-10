74a53c210c15c915d7ecc3e1bb10997c63b13dcb
binarydevelop <binarydevelop@gmail.com>
2020-05-10 16:04:02 +0530

    Added Solution For FSQRT

---
+++ Easy/FSQRT.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int T;
+    cin>>T;
+    int N;
+    while(T>0){
+       cin>>N;
+       cout<<sqrt(N);
+
+        T--;
+    }
+    return 0;
+}
