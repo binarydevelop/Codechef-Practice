c30b97fc3fd5f2a8955256f4cf7013a195484248
binarydevelop <binarydevelop@gmail.com>
2020-05-10 15:35:41 +0530

    Flow002

---
+++ Beginner/FLOW002.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+int main() {
+	// Read the number of test cases.
+	int t;
+	cin>>t;
+	for(int i=0;i<t;i++)
+	{
+	    int a,b;
+	    cin>>a>>b;
+	    cout<<(a%b)<<endl;
+	}
+	return 0;
+}
