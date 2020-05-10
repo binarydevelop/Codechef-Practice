e01cf48d33528f3542015e715703065a72fd8b5e
binarydevelop <binarydevelop@gmail.com>
2020-05-10 16:34:45 +0530

    Horses.cpp Solution

--- Easy/HORSES.cpp
+++ Easy/HORSES.cpp
@@ -9,13 +9,21 @@ int main(){
     while(T>0){
        cin>>N;
        long long int input;
+       int diff;
        vector<long long int> si;
        for(int i=0;i<N;i++){
            cin>>input;
            si.push_back(input);
        }
-        sort(a.begin(),a.end());
-        cout<<si[0]<<" "<<si[1];
+        sort(si.begin(),si.end());
+        diff=si[1]-si[0];
+        for(int j=2;j<N;j++){
+            if(si[j]-si[j-1]<diff){
+                diff=si[j]-si[j-1];
+            }
+            
+        }
+        cout<<diff<<"\n";
         T--;
     }
     return 0;
