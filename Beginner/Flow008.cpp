68f0737b3eaff196b6140610f9def762f95ed2e4
Tushar Roy <binarydevelop@gmail.com>
2021-10-21 17:33:26 +0530

    FLOW008

---
+++ Beginner/Flow008.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        if(N < 10){
+            cout<<"Thanks for helping Chef!"<<"\n";
+        }
+        else{
+            cout<<"-1"<<"\n";
+        }
+    }
+    
+    return 0;
+}
